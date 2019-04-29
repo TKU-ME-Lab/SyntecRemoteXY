058528255eac1e1d52f0d81eb65d95b4a35fa96f
mike9100255 <mike910025@gmail.com>
2019-04-29 20:08:33 +0800

    add annotation

--- SyntecRemoteXY/Form1.cs
+++ SyntecRemoteXY/Form1.cs
@@ -33,15 +33,9 @@ namespace SyntecRemoteXY
         private BitType m_bittype = BitType.R;
         private BitMode m_bitmode = BitMode.Read;
 
-        static NetworkStream m_NetStream;
-
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void m_buttonStatus_Click(object sender, EventArgs e)
-        {
         }
 
         private void m_buttonConnect_Click(object sender, EventArgs e)
@@ -49,8 +43,8 @@ namespace SyntecRemoteXY
             String host = m_textBoxIP.Text;
             if(state == ConnectionState.Closed)
             {
-                CNC = new SyntecRemoteCNC(host, 10000);
-                if (!CNC.isConnected())
+                CNC = new SyntecRemoteCNC(host, 10000);     //連接Syntec控制器 host: 控制器IP位址 ,port: 10000，EX:SyntecRemoteCNC("192.168.0.10",10000)
+                if (!CNC.isConnected())     //檢查連線狀態，連上回傳1，反之回傳0
                 {
                     m_listBoxLog_ShowLog("Can not connect:" + host);
                     m_buttonConnect.BackColor = SystemColors.Control;
@@ -148,9 +142,9 @@ namespace SyntecRemoteXY
                 int Rend = 27;
 
                 int[] data;
-                short result = CNC.READ_plc_register(Rstart, Rend, out data);
+                short result = CNC.READ_plc_register(Rstart, Rend, out data);       //取得PLC Register 位址資料，Rstart:位址起始值, Rend:位址終止值, data:Plc資料(int)
 
-                if (result != (short)SyntecRemoteCNC.ErrorCode.NormalTermination)
+                if (result != (short)SyntecRemoteCNC.ErrorCode.NormalTermination)   //確認錯誤訊息 NormalTermination:完成作業，未發生錯誤，回傳值0
                 {
                     m_listBoxLog_ShowLog("Fail Read Position");
                 }
@@ -175,9 +169,9 @@ namespace SyntecRemoteXY
                     {
                         case BitType.I:
                             byte[] data;
-                            result = CNC.READ_plc_ibit(no, no, out data);
+                            result = CNC.READ_plc_ibit(no,no, out data);  //取得PLC I Bit位址資料，no(第一個):位址起始值, no(第二個):位址終止值, data:Plc資料(byte)
 
-                            if (result != (short)SyntecRemoteCNC.ErrorCode.NormalTermination)
+                            if (result != (short)SyntecRemoteCNC.ErrorCode.NormalTermination)   //確認錯誤訊息 NormalTermination:完成作業，未發生錯誤，回傳值0
                             {
                                 m_listBoxLog_ShowLog("Fail Read I Bit");
                             }
@@ -189,9 +183,9 @@ namespace SyntecRemoteXY
                             break;
                         case BitType.R:
                             int[] Rdata;
-                            result = CNC.READ_plc_register(no, no, out Rdata);
+                            result = CNC.READ_plc_register(no,no, out Rdata);   //取得PLC Register 位址資料，Rstart:位址起始值, Rend:位址終止值, data:Plc資料(int)
 
-                            if (result != (short)SyntecRemoteCNC.ErrorCode.NormalTermination)
+                            if (result != (short)SyntecRemoteCNC.ErrorCode.NormalTermination)   //確認錯誤訊息 NormalTermination:完成作業，未發生錯誤，回傳值0
                             {
                                 m_listBoxLog_ShowLog("Fail Read Position");
                             }
@@ -202,9 +196,9 @@ namespace SyntecRemoteXY
                             }
                             break;
                         case BitType.C:
-                            result = CNC.READ_plc_cbit(no, no, out data);
+                            result = CNC.READ_plc_cbit(no, no, out data);   //取得PLC C Bit位址資料，no(第一個):位址起始值, no(第二個):位址終止值, data:Plc資料(byte)
 
-                            if (result != (short)SyntecRemoteCNC.ErrorCode.NormalTermination)
+                            if (result != (short)SyntecRemoteCNC.ErrorCode.NormalTermination)   //確認錯誤訊息 NormalTermination:完成作業，未發生錯誤，回傳值0
                             {
                                 m_listBoxLog_ShowLog("Fail Read C Bit");
                             }
@@ -225,41 +219,47 @@ namespace SyntecRemoteXY
                 }
             }
         }
-        string[] OldAlmMsg = {""};
+        System.DateTime[] OldAlmtime = { };
         private void m_timerstatus_Tick(object sender, EventArgs e)
         {
             if (state == ConnectionState.Connecting)
             {
-                string Mainprog;    //主程式
-                string Curprog;     //現在在執行的程式
-                int CurSeq;         //用不到
-                bool IsAlarm;       //確認警告訊號
-                string m_Mode;      //控制器模式 EX: 手動、自動執行....
-                string m_Status;    //控制器狀態
-                string m_Alarm;     //警報
-                string m_EMG;       //緊急停止
-                string[] AlmMsg;    //警告訊息
+                string Mainprog;    
+                string Curprog;     
+                int CurSeq;         
+                string Mode;      
+                string Status;    //控制器狀態，有兩種狀態 Ready 和 NotReady
+                string Alarm;     
+                bool IsAlarm;       //判斷是否有警報
+                string EMG;       
+                string[] AlmMsg;    //取得目前發生的警報訊息
+                System.DateTime[] Almtime;  //取得目前發生的警報時間
+                short result;
 
-                System.DateTime[] Almtime;
-
-                CNC.READ_status(out Mainprog, out Curprog, out CurSeq,
-                                                out m_Mode, out m_Status, out m_Alarm, out m_EMG);
-                CNC.READ_alm_current(out IsAlarm, out AlmMsg, out Almtime);
-
-                m_labelStatusVal.Text = m_Status;
-                if (IsAlarm)
+                result = CNC.READ_status(out Mainprog, out Curprog, out CurSeq,     //讀取狀態資訊，完成作業，回傳0
+                                        out Mode, out Status, out Alarm, out EMG);
+                if(result == (short)SyntecRemoteCNC.ErrorCode.NormalTermination)    //確認錯誤訊息 NormalTermination:完成作業，未發生錯誤，回傳值0
                 {
-                    m_timerAlarm.Start();
-                    if (String.Concat(OldAlmMsg) != String.Concat(AlmMsg))
-                    {
-                        m_listBoxLog_ShowLog(String.Concat(AlmMsg));
-                    }
-                    OldAlmMsg = AlmMsg;
+                    m_labelStatusVal.Text = Status;
                 }
-                else
+
+                result = CNC.READ_alm_current(out IsAlarm, out AlmMsg, out Almtime);    //目前發生的警報，完成作業，回傳0
+                if (result == (short)SyntecRemoteCNC.ErrorCode.NormalTermination)   //確認錯誤訊息 NormalTermination:完成作業，未發生錯誤，回傳值0
                 {
-                    m_timerAlarm.Stop();
-                    m_buttonConnect.BackColor = Color.Lime;
+                    if (IsAlarm)    //判斷是否有警報
+                    {
+                        m_timerAlarm.Start();
+                        if (String.Concat(OldAlmtime) != String.Concat(Almtime))
+                        {
+                            m_listBoxLog_ShowLog(String.Concat(AlmMsg));
+                        }
+                        OldAlmtime = Almtime;
+                    }
+                    else
+                    {
+                        m_timerAlarm.Stop();
+                        m_buttonConnect.BackColor = Color.Lime;
+                    }
                 }
             }
         }
@@ -269,10 +269,14 @@ namespace SyntecRemoteXY
             if(state == ConnectionState.Connecting)
             {
                 byte[] data = {0};
+                /*********寫入PLC C BIT位址資料*************/
+                //37(第一個):位址起始值, 37(第二個):位址終止值, data:Plc資料(byte)
+                //C37:外界對CNC發出RESET指令，1: 觸發C37，0: 重設C37, C37定義根據'PLC元件說明手冊'
                 data[0] = 1;
-                CNC.WRITE_plc_cbit(37, 37, data);
+                CNC.WRITE_plc_cbit(37, 37, data);   
                 data[0] = 0;
                 CNC.WRITE_plc_cbit(37, 37, data);
+                /**********寫入PLC C BIT位址資料************/
                 m_listBoxLog_ShowLog("STOP!!!!!!!!!!!!!!!");
             }
             else
@@ -303,16 +307,16 @@ namespace SyntecRemoteXY
                     switch (m_bittype)
                     {
                         case BitType.I:
-                            CNC.WRITE_plc_ibit(no, no, data);
+                            CNC.WRITE_plc_ibit(no, no, data);   //寫入PLC I BIT位址資料，no(第一個):位址起始值, no(第二個):位址終止值, data:Plc資料(byte)
                             m_listBoxLog_ShowLog("Write I" + no + ": " + data[0]);
                             break;
                         case BitType.R:
                             
-                            CNC.WRITE_plc_register(no, no, Rdata);
+                            CNC.WRITE_plc_register(no, no, Rdata);  //寫入PLC Register BIT位址資料，no(第一個):位址起始值, no(第二個):位址終止值, Rdata:Plc資料(int)
                             m_listBoxLog_ShowLog("Write R" + no + ": " + Rdata[0]);
                             break;
                         case BitType.C:
-                            CNC.WRITE_plc_cbit(no, no, data);
+                            CNC.WRITE_plc_cbit(no, no, data);   //寫入PLC C BIT位址資料，no(第一個):位址起始值, no(第二個):位址終止值, data:Plc資料(byte)
                             m_listBoxLog_ShowLog("Write C" + no + ": " + data[0]);
                             break;
                         default:
